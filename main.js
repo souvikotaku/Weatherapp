001e73fbbe17658f8f6d3ab066394864f13e05a2
Souvik Das <71898027+souvikotaku@users.noreply.github.com>
2020-11-28 19:16:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var button = document.querySelector(".clicksub");
 var wName = document.querySelector(".name");
 var wDesc = document.querySelector(".desc");
 var wTemp = document.querySelector(".temp");
- 
+var wIcon = document.querySelector("#image");
 
 button.addEventListener('click',()=>{
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=7353020ee6b5ea4e932e98307c3b700e')
@@ -12,6 +12,8 @@ button.addEventListener('click',()=>{
         wName.innerHTML = data['name'];
         wDesc.innerHTML = data['weather'][0]['description'];
         wTemp.innerHTML = data['main']['temp']+'&#8451';
+        var iconCode = data['weather'][0]['icon'];
+        wIcon.innerHTML = "<img src='http://openweathermap.org/img/wn/"+iconCode+"@2x.png'>";
      })
 .catch(err=>alert("wrong city name!"))
 
