106525ad27d3987ccfbd14d1f1a1aa8c4b243d42
Souvik Das <71898027+souvikotaku@users.noreply.github.com>
2020-11-28 16:13:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var inputValue = document.querySelector(".inputVal");
+var button = document.querySelector(".clicksub");
+var wName = document.querySelector(".name");
+var wDesc = document.querySelector(".desc");
+var wTemp = document.querySelector(".temp");
+ 
+
+button.addEventListener('click',()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=7353020ee6b5ea4e932e98307c3b700e')
+     .then(Response=>Response.json())
+     .then(data=> {
+        wName.innerHTML = data['name'];
+        wDesc.innerHTML = data['weather'][0]['description'];
+        wTemp.innerHTML = data['main']['temp']+'&#8451';
+     })
+.catch(err=>alert("wrong city name!"))
+
+ 
+})
+
